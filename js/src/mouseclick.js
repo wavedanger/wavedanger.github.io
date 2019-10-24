604bcea18583c818cf9e275af0cc12fc4bd83882
hongwb <weibinhong2018@163.com>
2019-10-24 11:55:37 +0800

    Site updated: 2019-10-24 11:55:27

--- js/src/mouseclick.js
+++ js/src/mouseclick.js
@@ -3,7 +3,7 @@ var a_idx = 0;
 var rgbM = "green";
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("瓦富强", "瓦民主", "瓦文明", "瓦和谐", "瓦自由", "瓦平等", "瓦公正" ,"瓦法治", "瓦爱国", "瓦敬业", "瓦诚信", "瓦友善");
+        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
         var rgbM='#'+Math.floor(Math.random()*0xffffff).toString(16);
 		var $i = $("<span />").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
